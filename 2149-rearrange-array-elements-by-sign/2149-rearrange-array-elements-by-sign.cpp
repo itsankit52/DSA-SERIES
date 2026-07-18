25bb28e2a484798f67fa32a6f4d98bd34515653f
ANKIT THAKUR <ankitthakur960ak@gmail.com>
2026-07-18 15:42:05 +0530

    Time: 6 ms (59.72%), Space: 128.3 MB (78.21%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        vector<int> ans(nums.size());
+
+        int positive = 0, negative = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                ans[positive] = nums[i];
+                positive += 2;
+            } else {
+                ans[negative] = nums[i];
+                negative += 2;
+            }
+        }
+        return ans;
+
+        // TIME COMPLEXITY : O(n)
+        // SPACE COMPLEXITY : O(n)
+    }
+};
