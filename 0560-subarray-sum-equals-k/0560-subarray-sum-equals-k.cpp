487c595152ab4d36dbce65f1e1c1ab012e67b8ea
ANKIT THAKUR <ankitthakur960ak@gmail.com>
2026-07-26 07:54:10 +0530

    Time: 39 ms (72.51%), Space: 45.3 MB (78.26%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
 
-        // Brute force : Time Complexity O(n^2)
+        // Brute force : Time Complexity -> O(n^2), Space Complexity -> O(n)
+        /*
         int count = 0;
         for (int i =0 ; i< nums.size(); i++) {
             int sum = 0;
@@ -11,9 +12,31 @@ public:
 
                 if (sum == k )
                     count ++;
-                
+
             }
         }
+        return count;
+        */
+
+        // Optimal Approach : TIme & Space Complexity -> O(n)
+        unordered_map<int, int> mpp;
+        int preSum = 0,count = 0;
+
+        // prefix sum 0 has occurred once
+        mpp[0] = 1;
+
+        for (int num : nums) {
+            // Update prefix sum
+            preSum += num;
+
+            // if there is a previous prefix sum
+            if (mpp.find(preSum - k) != mpp.end()) 
+                count += mpp[preSum - k];
+
+            // Store current prefix sum
+            mpp[preSum]++;
+        }
+
         return count;
     }
 };
