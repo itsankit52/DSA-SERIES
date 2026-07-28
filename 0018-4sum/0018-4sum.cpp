7230cdcc061559146181c1bef10cce9174e10d49
ANKIT THAKUR <ankitthakur960ak@gmail.com>
2026-07-28 13:22:35 +0530

    Time: 23 ms (54.02%), Space: 17.4 MB (85.03%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,31 +3,80 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
         // Optimal Approach (using two pointer) : Time Complexity -> O(n^3)
+        // Sort the array first 
+        sort(nums.begin(), nums.end());
 
-        // Better Approach (using hashset) : Time Complexity -> O(n^3)
         int n = nums.size();
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
 
         for (int i = 0; i < n - 3; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
             for (int j = i + 1; j < n - 2; j++) {
-                unordered_set<long long> st;
 
-                for (int k = j + 1; k < n ; k++) {
-                    long long l = (long long)target -
-                                  ((long long)nums[i] + nums[j] + nums[k]);
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
-                    if (st.find(l) != st.end()) {
-                        vector<int> temp;
-                        temp = {nums[i], nums[j], nums[k], (int)l};
+                int left = j + 1,right = n - 1;
 
-                        sort(temp.begin(), temp.end());
-                        ans.insert(temp);
-                    }
-                    st.insert(nums[k]);
+                while (left < right) {
+
+                    long long sum =
+                        (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+
+                        ans.push_back(
+                            {nums[i], nums[j], nums[left], nums[right]});
+
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+
+                    } 
+                    else if (sum < target)
+                        left++;
+                    else
+                        right--;
                 }
             }
         }
-        return vector<vector<int>>(ans.begin(), ans.end());
+
+        return ans;
+
+        /*
+                // Better Approach (using hashset) : Time Complexity -> O(n^3)
+                set<vector<int>> ans;
+
+                for (int i = 0; i < nums.size() - 3; i++) {
+                    for (int j = i + 1; j < nums.size() - 2; j++) {
+                        unordered_set<long long> st;
+
+                        for (int k = j + 1; k < nums.size(); k++) {
+                            long long l = (long long)target -
+                                          ((long long)nums[i] + nums[j] +
+           nums[k]);
+
+                            if (st.find(l) != st.end()) {
+                                vector<int> temp;
+                                temp = {nums[i], nums[j], nums[k], (int)l};
+
+                                sort(temp.begin(), temp.end());
+                                ans.insert(temp);
+                            }
+                            st.insert(nums[k]);
+                        }
+                    }
+                }
+                return vector<vector<int>>(ans.begin(), ans.end());
+        */
 
         // Brute Force (using four loops): Time Complexity -> O(n^4) gives TLE
         /*
